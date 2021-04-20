61424bc14283723e791fdcf725b6c75e7a3f1b51
tomires <me@tomires.eu>
2021-04-20 22:03:13 +0200

    Add ability to load gpx files and archives containing them

---
+++ Assets/Scripts/GpxLoader.cs
@@ -0,0 +1,44 @@
+using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
+
+public static class GpxLoader
+{
+    private static string ZipFiletype => NativeFilePicker.ConvertExtensionToFileType("zip");
+    private static string GpxFiletype => NativeFilePicker.ConvertExtensionToFileType("gpx");
+
+    public static void LoadGpx()
+    {
+        NativeFilePicker.PickFile(ProcessFile, 
+            new string[] { GpxFiletype, ZipFiletype });
+    }
+
+    private static void ProcessFile(string path)
+    {
+        if (string.IsNullOrEmpty(path)) return;
+        string ext = path.Split('.').Last();
+        bool isArchive = ext == "zip";
+
+        if(isArchive)
+        {
+            Directory.CreateDirectory(Constants.tempDirectory);
+            ZipUtil.Unzip(path, Constants.tempDirectory);
+            Directory.GetFiles(Constants.tempDirectory).ToList().ForEach(ProcessFile);
+            Directory.Delete(Constants.tempDirectory, true);
+            return;
+        }
+
+        if (ext != "gpx") return;
+        var cacheCode = GetCacheCode(path);
+        File.Copy(path, Constants.CacheFile(cacheCode), true);
+    }
+
+    private static string GetCacheCode(string path)
+    {
+        var serializer = new XmlSerializer(typeof(GpxStructure));
+        var reader = new StreamReader(path);
+        var gpx = serializer.Deserialize(reader) as GpxStructure;
+        reader.Close();
+        return gpx.wpt.code;
+    }
+}
---
+++ Assets/Scripts/GpxStructure.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Xml;
+using System.Xml.Serialization;
+
+[XmlRoot("gpx", Namespace = Constants.xmlRootNamespace)]
+public class GpxStructure
+{
+    public Waypoint wpt;
+}
+
+public class Waypoint
+{
+    [XmlAttribute] public string lat;
+    [XmlAttribute] public string lon;
+
+    [XmlElement("name")]
+    public string code;
+
+    [XmlElement("cache", Namespace = Constants.xmlNamespace)]
+    public CacheDetails details;
+}
+
+public class CacheDetails
+{
+    [XmlElement("name", Namespace = Constants.xmlNamespace)]
+    public string name;
+
+    [XmlElement("type", Namespace = Constants.xmlNamespace)]
+    public string type;
+
+    [XmlElement("container", Namespace = Constants.xmlNamespace)]
+    public string container;
+
+    [XmlElement("difficulty", Namespace = Constants.xmlNamespace)]
+    public float difficulty;
+
+    [XmlElement("terrain", Namespace = Constants.xmlNamespace)]
+    public float terrain;
+
+    [XmlElement("encoded_hints", Namespace = Constants.xmlNamespace)]
+    public string hint;
+
+    /*[XmlArrayItem("logs", Namespace = Constants.xmlNamespace)]
+    public List<Log> logs;*/
+}
+
+public class Log
+{
+    [XmlElement("date", Namespace = Constants.xmlNamespace)]
+    public DateTime date;
+
+    [XmlElement("type", Namespace = Constants.xmlNamespace)]
+    public string type;
+}
